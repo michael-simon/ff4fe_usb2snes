46c41cc80d0cb676e051eaf3c8f80a643e90178e
Michael Simon <mcsimon@gmail.com>
2022-02-26 13:45:05 -0600

    1.3.0 : lua bridge compatible objectives + flags

--- tracking_interface.js
+++ tracking_interface.js
@@ -53,9 +53,33 @@ function tracking_interface() {
     module.auto_set_live_objectives(module.objectives);
   }
 
+
+
   module.objectives = null;
+  module.flags = null; // WARNING: Flags will be <hidden> on a mystery seed
   module.get_objectives_from_metadata = get_objectives_from_metadata;
   function get_objectives_from_metadata() {
+      network_objectives.snes.send(JSON.stringify({
+         "Opcode" : "GetAddress",
+         "Space" : "SNES",
+         "Operands": ["0x1FF000", '400']
+      })).then(
+        (event) => {
+         return event.data.arrayBuffer()
+       }).then(
+       (metadata) => {
+         let x = new Uint8Array(metadata);
+         let bytes = x[0] + 256 * x[1];
+         let meta = new TextDecoder("utf-8").decode(x.slice(4,bytes+4));
+         module.objectives = JSON.parse(meta).objectives;
+         module.flags = JSON.parse(meta).flags.toUpperCase();
+         module.set_live_objectives();
+         return;
+     });
+  }
+
+  module.get_objectives_from_file_metadata = get_objectives_from_file_metadata;
+  function get_objectives_from_file_metadata() {
     module.network_objectives.snes.send(module.network_objectives.snes.create_message("Info")
   ).then((out) => {
      let infoArray = JSON.parse(out.data).Results;
@@ -66,6 +90,7 @@ function tracking_interface() {
        module.network_objectives.snes.create_message("GetFile",[filename]))
    }).then((metadata) => {
      module.objectives = JSON.parse(metadata).objectives;
+     module.flags = JSON.parse(metadata).flags.toUpperCase();
      module.set_live_objectives();
      return;
    });
